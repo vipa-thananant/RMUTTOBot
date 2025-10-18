61ea285eb8168896acf778e01314a3762b8ed6ee
vipa-thananant <vipa@cpc.ac.th>
2025-10-18 11:33:57 +0700

    Add files via upload

---
+++ lib/MatchResult.dart
@@ -0,0 +1,13 @@
+class MatchResult {
+  final String question;
+  final double score;
+
+  MatchResult({required this.question, required this.score});
+
+  factory MatchResult.fromJson(Map<String, dynamic> json) {
+    return MatchResult(
+      question: json['question'],
+      score: json['score'],
+    );
+  }
+}
